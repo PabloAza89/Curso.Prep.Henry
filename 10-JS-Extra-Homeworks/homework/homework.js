d1f64ae6660c36ca39043c6b173abae62d65982c
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2022-04-10 23:14:21 -0300

    Homework 10-JS-Extra-Homeworks finishied

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,12 +10,16 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
-  var array = [];
 
-  for (var clave in objeto) {
-    array.push([clave, objeto[clave]]);
+  function convierteAmatriz(objeto) {  
+    var array = [];
+
+    for (var clave in objeto) {
+      array.push([clave, objeto[clave]]);
+    }
+    return array;
   }
-  return array;
+  return convierteAmatriz(objeto);
 }
 
 function numberOfCharacters(string) {
@@ -23,29 +27,40 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
-  var resultado = {};
 
-  string.split('').forEach( 
-    function(i) {
-      resultado[i] ? resultado[i]++ : resultado[i] = 1;
-    }
-  )
-  return resultado;
+  function cuantosCaracteres(string) {
+    var resultado = {};
+
+    string.split('').forEach( 
+      function(i) {
+        resultado[i] ? resultado[i]++ : resultado[i] = 1;
+      }
+    )
+    return resultado;
+  }
+  return cuantosCaracteres(string);  
 }
 
 function capToFront(s) {
   //Realiza una función que reciba como parámetro un string y mueva todas las letras mayúsculas
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
-  //Ejemplo: DESArrollaDOR -> DESADORrrolla
   //Escribe tu código aquí
   
-  const resultado = [...s]
-  .sort((a, b) => /[A-Z]/.test(a) ? /[A-Z]/.test(b) ? 0 : -1 : 0).join('');
+  function MayusculasAlPrincipio(s) {
+    var mayuscula = '';
+    var minuscula = '';
 
-return resultado;
+    for (var i = 0 ; i < s.length ; i++) {
+      if (s[i] === s[i].toUpperCase()) {
+        mayuscula += s[i];
+      }
+      else minuscula += s[i];
+    }  
+    return mayuscula + minuscula;
+  }
+  return MayusculasAlPrincipio(s);
 }
-
 
 function asAmirror(str) {
   //La función recibe una frase. 
@@ -53,30 +68,58 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
-} 
 
+  function palabrasAlReves(str) {
+    var arr = str.split(' ').map(palabra => palabra.split('').reverse().join(''));
+    return arr.join(' ');
+  }
+  return palabrasAlReves(str);
+} 
 
 function capicua(numero){
   //Escribe una función, la cual recibe un número y determina si es o no capicúa.
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
-}
 
+  function esCapicua(numero) {
+    if (numero.toString() === numero.toString().split('').reverse().join('')) {
+      return 'Es capicua';
+    }
+    return 'No es capicua';
+  }
+  return esCapicua(numero);
+}
 
 function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
-}
 
+  function eliminaLetras(cadena) {
+
+    var filtro1ro = cadena.split('a').join('');
+    var filtro2do = filtro1ro.split('b').join('');
+    var filtro3ro = filtro2do.split('c').join('');
+   
+    return filtro3ro;
+  }
+  return eliminaLetras(cadena);
+}
 
 function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  
+  function ordenaMenorAmayor(arr) {
+    arr.sort(function (a, b) {
+      return a.length - b.length;
+    });
+    return arr;
+  }
+  return ordenaMenorAmayor(arr);
 }
-
 
 function buscoInterseccion(arreglo1, arreglo2){
   //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita 
@@ -84,9 +127,21 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+    
+  function enComun(arreglo1, arreglo2) {
+    var resultado = [];
+  
+    for (var i = 0 ; i < arreglo1.length ; i++) {
+      for (var j = 0 ; j < arreglo2.length ; j++) {
+        if (arreglo1[i] === arreglo2[j]) {
+          resultado.push(arreglo2[j]);
+        }
+      }
+    }
+    return resultado;
+  }
+  return enComun(arreglo1, arreglo2);  
 }
-
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
