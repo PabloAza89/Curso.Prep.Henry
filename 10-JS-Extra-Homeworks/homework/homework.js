23347e3602a8cb6d24e336cdf978035f202cd211
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2022-04-10 23:23:07 -0300

    Homework 10-JS-Extra-Homeworks finishied

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -46,7 +46,6 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
-<<<<<<< HEAD
   
   function MayusculasAlPrincipio(s) {
     var mayuscula = '';
@@ -61,10 +60,6 @@ function capToFront(s) {
     return mayuscula + minuscula;
   }
   return MayusculasAlPrincipio(s);
-=======
-  var resultado = [...s].sort((a, b) => /[A-Z]/.test(a) ? /[A-Z]/.test(b) ? 0 : -1 : 0).join('');
-  return resultado;
->>>>>>> a12ac7dc803c795dbdacb47de96bdb00532e5f89
 }
 
 function asAmirror(str) {
